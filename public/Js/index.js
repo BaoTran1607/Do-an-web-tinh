33c52cc196d934e18957a349e48113a7b5a80e08
ThienBao <tdtb1001@gmail.com>
2023-12-13 18:59:47 +0700

    init 10: - Them trang chi tiet(WIP) - Them nut xem them

--- public/Js/index.js
+++ public/Js/index.js
@@ -1,6 +1,8 @@
 import { getUploads } from "./services/upload.service.js";
 
 const navigationEl = document.querySelector(".navigation");
+const viewmoreBtn = document.querySelector(".recent-viewmore");
+const recentContainerEl = document.querySelector(".recent-container");
 
 const navScroll = navigationEl.offsetTop;
 
@@ -15,6 +17,7 @@ getUploads().then(function (uploads) {
       </div>`;
   }
 });
+
 window.onscroll = function(){checkStickyNav();}
 
 function checkStickyNav(){
@@ -26,3 +29,37 @@ function checkStickyNav(){
     navigationEl.classList.add("nonstick-nav");
   }
 }
+
+function recentViewmore(){
+  for(let i = 0; i <= 8; i++){
+    recentContainerEl.innerHTML += `<div class="recent-card">
+                    <div class="recent-img">
+                      <a class="recent-img-link" href="">
+                        <img class="recent-img"
+                          src="https://gamek.mediacdn.vn/zoom/310_200/133514250583805952/2023/12/9/avatar1702112830521-17021128308761221146901-0-38-280-486-crop-17021129700721075675624.jpg"
+                        />
+                      </a>
+                    </div>
+
+                    <div class="recent-card-text">
+                      <div class="recent-title">
+                        <a href="">
+                          Nhá hàng nhân vật mới tại TGA, Honkai: Star Rail khiến
+                          fan điêu đứng hơn cả việc giật giải
+                        </a>
+                      </div>
+                      <div class="recent-subtitle">
+                        Xuất hiện chỉ vỏn vẹn vài giây, thế nhưng nhân vật này
+                        đã khiến cả cộng đồng Honkai: Star Rail phải mê mẩn.
+                      </div>
+                      <div class="recent-info">
+                        <a href="" class="recent-author">Thế Anh</a> -
+                        <span>Mobile & Social</span> <span>09/12/2023</span>
+                        <span>18:32</span>
+                      </div>
+                    </div>
+                  </div>`;
+  }
+}
+
+viewmoreBtn.addEventListener("click", recentViewmore, false);
