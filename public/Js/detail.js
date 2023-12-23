5a9cab6de5ecb86f52bd37157fbeaedc96951d2f
ThienBao <tdtb1001@gmail.com>
2023-12-23 21:20:30 +0700

    Init 14: - Them responsive cho header

--- public/Js/detail.js
+++ public/Js/detail.js
@@ -1,6 +1,8 @@
 
 const navigationEl = document.querySelector(".navigation");
 const relatedEl = document.querySelector(".related-container");
+const viewmoreBtn = document.querySelector(".recent-viewmore");
+const recentContainerEl = document.querySelector(".recent-container");
 
 const navScroll = navigationEl.offsetTop;
 const relatedScroll = relatedEl.offsetTop;
@@ -27,3 +29,36 @@ function checkStickyRelated(){
         relatedEl.classList.remove("sticky-related");
     }
 }
+
+function recentViewmore() {
+  for (let i = 0; i <= 8; i++) {
+    recentContainerEl.innerHTML += `<div class="recent-card">
+                    <div class="recent-img">
+                      <a class="recent-img-link" href="">
+                        <img class="recent-img"
+                          src="https://gamek.mediacdn.vn/zoom/310_200/133514250583805952/2023/12/21/vvv-1703152321167607292221-0-0-459-734-crop-17031523259371493211599.png"
+                        />
+                      </a>
+                    </div>
+
+                    <div class="recent-card-text">
+                      <div class="recent-title">
+                        <a href="">
+                          Bom tấn của Capcom tiếp tục "bay màu" sau nhiều lần gắng gượng, có lẽ đã đến lúc kết thúc 
+                          một huyền thoại
+                        </a>
+                      </div>
+                      <div class="recent-subtitle">
+                        Sau nhiều nỗ lực “vớt vát”, cuối cùng Capcom cũng đưa ra quyết định này.
+                      </div>
+                      <div class="recent-info">
+                        <a href="" class="recent-author">Thế Anh</a> -
+                        <span>Mobile & Social</span> <span>21/12/2023</span>
+                        <span>20:03</span>
+                      </div>
+                    </div>
+                  </div>`;
+  }
+}
+
+viewmoreBtn.addEventListener("click", recentViewmore, false);
